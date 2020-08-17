63b8dd395eb0d809c2ffe71e815f9ade67eb964e
Jpendy <Jpendy256@gmail.com>
2020-08-16 20:39:00 -0700

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom';
 import HomePage from './homePage/HomePage';
 import ProjectDetailPage from './projectDetailPage/ProjectDetailPage';
+import Header from './header/Header';
 
 export default function App() {
  
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}  />
         <Route exact path='/:id' component={ProjectDetailPage} /> 
--- src/components/App/projectList/ProjectList.jsx
+++ src/components/App/projectList/ProjectList.jsx
@@ -4,17 +4,17 @@ import projectArray from '../../../data/data';
 
 export default function ProjectList() {
 
-  const listElements = projectArray.map(({ name, image, summary, id }, i) => {
+  const listElements = projectArray.map(({ name, image, summary, id, siteLink, gitHubLink }, i) => {
     return (
-       
       <li key={i} >
         <Link to={`/${id}`} >
           <img src={image} />
           <h3>{name}</h3>
         </Link>
+        <a href={siteLink} >{siteLink}</a>
+        <a href={gitHubLink} >{gitHubLink}</a>
         <p>{summary}</p>
-      </li>
-     
+      </li> 
     );
   });
 
